f7b4de949b0e483cec13e8d30f860f21e7e92987
marload <rladhkstn8@gmail.com>
2020-03-23 21:53:27 +0900

    DRQN Working

--- DRQN/drqn_discrete_action.py
+++ DRQN/drqn_discrete_action.py
@@ -1,5 +1,3 @@
-# NOTE This model may not be working properly yet.
-
 import wandb
 import tensorflow as tf
 from tensorflow.keras.layers import Input, Dense, LSTM, Lambda
@@ -15,129 +13,138 @@ wandb.init(name='DRQN', project="deep-rl-tf2")
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--gamma', type=float, default=0.95)
-parser.add_argument('--time_steps', type=int, default=4)
+parser.add_argument('--time_steps', type=int, default=3)
 parser.add_argument('--lr', type=float, default=0.005)
 parser.add_argument('--batch_size', type=int, default=32)
 parser.add_argument('--tau', type=float, default=0.125)
+parser.add_argument('--eps', type=float, default=1.0)
+parser.add_argument('--eps_decay', type=float, default=0.995)
+parser.add_argument('--eps_min', type=float, default=0.01)
+parser.add_argument('--train_start', type=int, default=100)
+parser.add_argument('--update_interval', type=int, default=5)
 
 args = parser.parse_args()
+
+
+class ActionValueModel:
+    def __init__(self, state_dim, action_dim):
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        
+        self.eps = args.eps
+        self.eps_decay = args.eps_decay
+        self.eps_min = args.eps_min
+
+        self.compute_loss = tf.keras.losses.MeanSquaredError()
+        self.opt = tf.keras.optimizers.Adam(args.lr)
+        self.model = self.create_model()
+
+    def create_model(self):
+        return tf.keras.Sequential([
+            Input((args.time_steps, self.state_dim)),
+            LSTM(128, activation='relu'),
+            Dense(128, activation='relu'),
+            Dense(128, activation='relu'),
+            Dense(self.action_dim)
+        ])
+
+    def predict(self, state):
+        return self.model.predict(state)
+
+    def get_action(self, state, training=True):
+        state = state.reshape(1, args.time_steps, self.state_dim)
+        self.eps *= self.eps_decay
+        self.eps = max(self.eps_min, self.eps)
+        eps = self.eps if training else 0.01
+        q_value = self.model.predict(state)[0]
+        if random.random() < eps:
+            return random.randint(0, self.action_dim-1)
+        return np.argmax(q_value)
+    
+    def train(self, state, target):
+        state = state.reshape(-1, args.time_steps, self.state_dim)
+        with tf.GradientTape() as tape:
+            logits = self.model(state, training=True)
+            assert logits.shape == target.shape
+            loss = self.compute_loss(tf.stop_gradient(target), logits)
+        grads = tape.gradient(loss, self.model.trainable_variables)
+        self.opt.apply_gradients(zip(grads, self.model.trainable_variables))
 
 
 class Agent:
     def __init__(self,
                  env,
-                 memory_cap=1000,
-                 eps=1.0,
-                 eps_decay=0.995,
-                 eps_min=0.01):
+                 memory_cap=1000):
         self.env = env
         self.memory = deque(maxlen=memory_cap)
-        self.state_dim = env.observation_space.shape[0]
-        self.action_dim = env.action_space.n
+        self.state_dim = self.env.observation_space.shape[0]
+        self.action_dim = self.env.action_space.n
         self.stored_states = np.zeros((args.time_steps, self.state_dim))
-
-        self.eps = eps
-        self.eps_decay = eps_decay
-        self.eps_min = eps_min
-
-        self.compute_loss = tf.keras.losses.MeanSquaredError()
-        self.opt = tf.keras.optimizers.Adam(args.lr)
-        self.model = self.create_model()
-        self.t_model = self.create_model()
-        self.t_model.set_weights(self.model.get_weights())
-
-    def create_model(self):
-        return tf.keras.Sequential([
-            Input((args.time_steps, self.state_dim)),
-            LSTM(128, activation='tanh'),
-            Dense(128, activation='relu'),
-            Dense(self.action_dim)
-        ])
-
-    def get_action(self, training=True):
-        states = np.expand_dims(self.stored_states, axis=0)
-        self.eps *= self.eps_decay
-        self.eps = max(self.eps_min, self.eps)
-        eps = self.eps if training else 0.01
-        q_values = self.model.predict(states)[0]
-        if np.random.normal() < eps:
-            return self.env.action_space.sample()
-        return np.argmax(q_values)
-
-    def put_memory(self, state, action, reward, new_state, done):
-        self.memory.append([state, action, reward, new_state, done])
+        self.model = ActionValueModel(self.state_dim, self.action_dim)
+        self.target_model = ActionValueModel(self.state_dim, self.action_dim)
+        self.update_count = 0
+        self.update_target_model()
+    
+    def update_target_model(self):
+        self.target_model.model.set_weights(self.model.model.get_weights())
 
     def update_states(self, next_state):
         self.stored_states = np.roll(self.stored_states, -1, axis=0)
         self.stored_states[-1] = next_state
+    
+    def reset_stored_state(self):
+        self.stored_states = np.zeros((args.time_steps, self.state_dim))
 
-    def target_update(self):
-        weights = self.model.get_weights()
-        t_weights = self.t_model.get_weights()
-        for i in range(len(t_weights)):
-            t_weights[i] = weights[i] * args.tau + \
-                t_weights[i] * (1 - args.tau)
-        self.t_model.set_weights(t_weights)
-
-    def train_step(self, x, y):
-        with tf.GradientTape() as tape:
-            logits = self.model(x, training=True)
-            assert logits.shape == y.shape
-            loss = self.compute_loss(tf.stop_gradient(y), logits)
-        grads = tape.gradient(loss, self.model.trainable_variables)
-        self.opt.apply_gradients(zip(grads, self.model.trainable_variables))
+    def put_memory(self, state, action, reward, new_state, done):
+        self.memory.append([state, action, reward, new_state, done])
 
     def replay(self):
         samples = random.sample(self.memory, args.batch_size)
         batch_states = []
         batch_target = []
         for sample in samples:
-            states, action, reward, next_states, done = sample
-            batch_states.append(states)
-            states = np.expand_dims(states, axis=0)
-            target = self.t_model.predict(states)[0]
+            state, action, reward, next_state, done = sample
+            state = state.reshape(args.time_steps, self.state_dim)
+            batch_states.append(state.reshape(args.time_steps, self.state_dim))
+            state = state.reshape((1, args.time_steps, self.state_dim))
+            target = self.target_model.predict(state)[0]
             train_reward = reward * 0.01
+            
             if done:
                 target[action] = train_reward
             else:
-                next_states = np.expand_dims(next_states, axis=0)
-                next_q_value = max(self.t_model.predict(next_states)[0])
+                next_state = next_state.reshape((1, args.time_steps, self.state_dim))
+                next_q_value = max(self.target_model.predict(next_state)[0])
                 target[action] = train_reward + next_q_value * args.gamma
             batch_target.append(target)
-        self.train_step(np.array(batch_states), np.array(batch_target))
-
+            self.model.train(np.array(batch_states), np.array(batch_target))
+            self.update_count = 0
+     
     def train(self, max_episodes=1000):
-        done, ep, total_reward = True, 0, 0
-        while ep <= max_episodes:
-            if done:
-                self.stored_states = np.zeros(
-                    (args.time_steps, self.state_dim))
-                print('EP{} EpisodeReward={}'.format(ep, total_reward))
-                wandb.log({'Reward': total_reward})
+        for ep in range(max_episodes):
+            done, total_reward = False, 0
+            self.reset_stored_state()
+            state = self.env.reset()
+            self.update_states(state)
+            while not done:
+                # self.env.render()
+                action = self.model.get_action(self.stored_states)
+                next_state, reward, done, _ = self.env.step(action)
+                prev_stored_states = self.stored_states
+                self.put_memory(prev_stored_states, action, reward, self.stored_states, done)
 
-                done, cur_state, total_reward = False, self.env.reset(), 0
-                self.update_states(cur_state)
-                ep += 1
-
-            action = self.get_action()
-            next_state, reward, done, _ = self.env.step(action)
-            prev_stored_states = self.stored_states
-            self.update_states(next_state)
-            self.put_memory(prev_stored_states, action,
-                            reward, self.stored_states, done)
-
-            if len(self.memory) >= args.batch_size:
-                self.replay()
-            self.target_update()
-
-            total_reward += reward
-
+                if len(self.memory) > args.train_start and args.update_interval < self.update_count:
+                    self.replay()
+                total_reward += reward
+                self.update_count += 1
+            self.update_target_model()
+            print('EP{} EpisodeReward={}'.format(ep, total_reward))
+            wandb.log({'Reward': total_reward})
 
 def main():
     env = gym.make('CartPole-v1')
     agent = Agent(env)
-    agent.train(max_episodes=1000)
-
+    agent.train()
 
 if __name__ == "__main__":
     main()
