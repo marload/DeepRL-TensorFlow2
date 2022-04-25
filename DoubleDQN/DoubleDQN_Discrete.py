8b231f23a52534836a1c3f5b992ac165f7719dba
flybywind@foxmail.com <flybywind@foxmail.com>
2022-04-25 20:12:50 +0800

    before off-police impl

--- DoubleDQN/DoubleDQN_Discrete.py
+++ DoubleDQN/DoubleDQN_Discrete.py
@@ -1,16 +1,14 @@
-import wandb
+from MazeEnv import Maze
 import tensorflow as tf
 from tensorflow.keras.layers import Input, Dense, Flatten, Lambda
 from tensorflow.keras.optimizers import Adam
 
-import gym
 import argparse
 import numpy as np
 from collections import deque
 import random
 
-tf.keras.backend.set_floatx('float64')
-wandb.init(name='DoubleDQN', project="deep-rl-tf2")
+tf.keras.backend.set_floatx('float32')
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--gamma', type=float, default=0.95)
@@ -18,7 +16,7 @@ parser.add_argument('--lr', type=float, default=0.005)
 parser.add_argument('--batch_size', type=int, default=32)
 parser.add_argument('--episode', type=int, default=32)
 parser.add_argument('--replay_batch', type=int, default=10)
-parser.add_argument('--eps', type=float, default=1.0)
+parser.add_argument('--eps', type=float, default=0.9)
 parser.add_argument('--eps_decay', type=float, default=0.995)
 parser.add_argument('--eps_min', type=float, default=0.01)
 
@@ -26,18 +24,26 @@ args = parser.parse_args()
 
 
 class ReplayBuffer:
-  def __init__(self, capacity=10000):
+  def __init__(self, capacity=64, sample_policy='recent'):
     self.buffer = deque(maxlen=capacity)
+    self.sample_policy = sample_policy
 
   def put(self, state, action, reward, next_state, done):
     self.buffer.append([state, action, reward, next_state, done])
 
   def sample(self):
-    sample = random.sample(self.buffer, args.batch_size)
+    bs = args.batch_size
+    if self.sample_policy == 'random':
+      sample = random.sample(self.buffer, args.batch_size)
+    if self.sample_policy == 'recent':
+      sample = list(map(lambda x: self.buffer.pop(), range(bs)))
+      if sample is None:
+        return [None]*5
     states, actions, rewards, next_states, done = map(np.asarray, zip(*sample))
-    states = np.array(states).reshape(args.batch_size, -1)
-    next_states = np.array(next_states).reshape(args.batch_size, -1)
+    states = np.array(states).reshape(bs, -1)
+    next_states = np.array(next_states).reshape(bs, -1)
     return states, actions, rewards, next_states, done
+
 
   def size(self):
     return len(self.buffer)
@@ -48,7 +54,7 @@ class QualityModel:
     self.state_dim = state_dim
     self.action_dim = aciton_dim
     self.epsilon = args.eps
-
+    self.updates = 0
     self.model = self.create_model()
 
   def create_model(self):
@@ -56,7 +62,7 @@ class QualityModel:
       Input((self.state_dim,)),
       Dense(32, activation='relu'),
       Dense(16, activation='relu'),
-      Dense(self.action_dim)
+      Dense(self.action_dim, activation="softmax")
     ])
     model.compile(loss='mse', optimizer=Adam(args.lr))
     return model
@@ -65,7 +71,6 @@ class QualityModel:
     return self.model.predict(state)
 
   def get_action(self, state):
-    state = np.reshape(state, [1, self.state_dim])
     self.epsilon *= args.eps_decay
     self.epsilon = max(self.epsilon, args.eps_min)
     q_value = self.predict(state)[0]
@@ -74,20 +79,21 @@ class QualityModel:
     return np.argmax(q_value)
 
   def train(self, states, targets):
+    self.updates += 1
     self.model.fit(states, targets, epochs=1, verbose=0)
 
 
 class Agent:
-  def __init__(self, env):
+  def __init__(self, env:Maze):
     self.env = env
-    self.state_dim = self.env.observation_space.shape[0]
-    self.action_dim = self.env.action_space.n
+    self.buffer = ReplayBuffer(args.batch_size*2)
+    self.state_dim = self.env.state_dim()
+    self.action_dim = self.env.action_dim()
 
     self.model = QualityModel(self.state_dim, self.action_dim)
     self.target_model = QualityModel(self.state_dim, self.action_dim)
     self.target_update()
 
-    self.buffer = ReplayBuffer()
 
   def target_update(self):
     weights = self.model.model.get_weights()
@@ -95,6 +101,8 @@ class Agent:
 
   def replay(self):
     for _ in range(args.replay_batch):
+      if self.buffer.size() < args.batch_size:
+        break
       states, actions, rewards, next_states, done = self.buffer.sample()
       targets = self.target_model.predict(states)
       next_q_values = self.target_model.predict(next_states)[:, np.argmax(self.model.predict(next_states), axis=1)]
@@ -106,21 +114,22 @@ class Agent:
       done, total_reward = False, 0
       state = self.env.reset()
       while not done:
+        self.env.print()
         action = self.model.get_action(state)
-        next_state, reward, done, _ = self.env.step(action)
-        self.buffer.put(state, action, reward * 0.01, next_state, done)
+        next_state, reward, done = self.env.action(action)
+        self.buffer.put(state, action, reward, next_state, done)
         total_reward += reward
         state = next_state
 
       if self.buffer.size() >= args.batch_size:
         self.replay()
       self.target_update()
-      print('EP{} EpisodeReward={}'.format(ep, total_reward))
-      wandb.log({'Reward': total_reward})
+      print('EP{} EpisodeReward={}, reward={}, updates = {}'.format(ep, total_reward, reward, self.model.updates))
 
 
 def main():
-  env = gym.make('CartPole-v1')
+  print(f"args = {args}")
+  env = Maze(5)
   agent = Agent(env)
   agent.train()
 
--- DoubleDQN/MazeEnv.py
+++ DoubleDQN/MazeEnv.py
@@ -4,6 +4,7 @@ import sys
 
 class Maze:
   def __init__(self, size):
+    self._step = 0
     self._size = size
     self._maze = np.zeros(shape=(size, size), dtype=np.int)
     # random select 2 blockers
@@ -11,25 +12,63 @@ class Maze:
     bk_x = random.sample(range(size), block_num)
     bk_y = random.sample(range(size), block_num)
     for x, y in zip(bk_x, bk_y):
-      self._maze[x, y] = -1
+      self._maze[x, y] = -2
     # ensure reachable:
+    self._maze[0, 0] = 0
     self._maze[-2, -1] = 0
     self._maze[-1, -2] = 0
-    self._maze[-1, -1] = 1
-    self._robot = [0, 0]
+    self._maze[-1, -1] = 2
+    self._robot = np.zeros(shape=(2,), dtype=np.int)
 
+  # return state
   def reset(self):
-    self._robot = [0, 0]
+    self._step = 0
+    self._robot = np.zeros(shape=(2,), dtype=np.int)
+    maze = self._maze.copy()
+    maze[self._robot[0], self._robot[1]] = 1
+    return maze.reshape(1, -1)
 
+  def state_dim(self):
+    return self._size**2
+
+  def action_dim(self):
+    return 4
+
+  # return next_state, reward, done
   def action(self, a):
-    self._robot[0] += a[0]
-    self._robot[1] += a[1]
+    if a == 0: # left
+      self._robot[1] -= 1
+    if a == 1: # right
+      self._robot[1] += 1
+    if a == 2: # down
+      self._robot[0] += 1
+    if a == 3: # up
+      self._robot[0] -= 1
+    if (self._robot < 0).any() or (self._robot >= self._size).any():
+      print(f"action = {a}, robot = {self._robot}, fallout of boarder ...")
+      return self._maze.reshape(1, -1), -2, True
+
+    maze = self._maze.copy()
+    r = maze[self._robot[0], self._robot[1]]
+    if r < 0:
+      print(f"action = {a}, robot = {self._robot}, fall into trap ...")
+    elif 0 <= r < 2:
+      maze[self._robot[0], self._robot[1]] = 1
+      r -= self._step / 100
+    elif r >= 2:
+      print(f"action = {a}, robot = {self._robot}, WIN!")
+    self._step += 1
+    if self._step >= 100 and r < 2:
+      r = -2
+      print(f"action = {a}, robot = {self._robot}, Too Slow, force quit!")
+
+    return maze.reshape(1, -1), r, (r == -2 or r == 2)
 
   def print(self):
     maze = self._maze.copy()
-    maze[self._robot[0], self._robot[1]] = 1
+    maze[self._robot[0], self._robot[1]] = -3
     for i in range(self._size):
-      line = " | ".join(f"{x:2d}" for x in maze[i,:])
+      line = " | ".join(f"{x:2d}" if x > -3 else f" *" for x in maze[i,:])
       print(f"{line}\n")
       print("_"*len(line)+"\n")
     print("*"*len(line)+"\n")
@@ -37,19 +76,19 @@ class Maze:
 
 if __name__ == "__main__":
   import time
-  maze = Maze(5)
+  maze = Maze(3)
   maze.print()
   time.sleep(2)
-  maze.action([1, 0])
+  maze.action(1)
   maze.print()
   time.sleep(2)
-  maze.action([0, 1])
+  maze.action(2)
   maze.print()
   time.sleep(2)
-  maze.action([1, 1])
+  maze.action(0)
   maze.print()
   time.sleep(2)
-  maze.action([-1, 0])
+  maze.action(3)
   maze.print()
 
 
